9da2f9705b881843921f9bdf2ed79047c580e761
HasinduBimsara <22cis0270@ms.sab.ac.lk>
2025-12-05 11:48:21 +0530

    Replace bcrypt with bcryptjs in dependencies for improved compatibility

--- index.js
+++ index.js
@@ -7,18 +7,19 @@ import verifyJWT from "./middleware/auth.js";
 import orderRouter from "./routes/orderRouter.js";
 import dotenv from "dotenv";
 import cors from "cors";
+
 dotenv.config();
 const app = express();
 app.use(cors());
 
 mongoose
-	.connect(process.env.MONGO_URL)
-	.then(() => {
-		console.log("Connected to the database");
-	})
-	.catch((err) => {
-		console.log("Connection failed");
-	});
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((err) => {
+    console.log("Connection failed");
+  });
 
 app.use(bodyParser.json());
 app.use(verifyJWT);
@@ -27,7 +28,6 @@ app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
 app.use("/api/order", orderRouter);
 
-
 app.listen(5000, () => {
-	console.log("Server is running on port 5000");
+  console.log("Server is running on port 5000");
 });
